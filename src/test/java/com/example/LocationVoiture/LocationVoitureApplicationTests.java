58896e91515c6f9c953fe75f3a942f4b5332439a
Yassine <yassine.ouri7319@gmail.com>
2023-01-22 20:47:09 +0100

    init project

---
+++ src/test/java/com/example/LocationVoiture/LocationVoitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LocationVoiture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVoitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
